1725a15677990f36394afa636b2b7604ac7128cb
Stamat <xsouvd2@gmail.com>
2024-02-21 12:37:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitHub_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+        }
+    }
+}
